456e09aaaf0c7b6469d0c367ed10cb6019860b91
Andrebg28 <andrebg28@gmail.com>
2019-01-17 09:15:00 -0200

    Ciação do metodo listagem_2a, inspirado na Listagem 2

--- exemplos_e_artigos/reflection_and_notation/src/main/java/br/com/andrebg/apendendo/notation/App.java
+++ exemplos_e_artigos/reflection_and_notation/src/main/java/br/com/andrebg/apendendo/notation/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	ClienteReflection.listagem2();
+    	ClienteReflection.listagem_2();
+    	ClienteReflection.listagem_2a();
     }
 }
--- exemplos_e_artigos/reflection_and_notation/src/main/java/br/com/andrebg/apendendo/notation/ClienteReflection.java
+++ exemplos_e_artigos/reflection_and_notation/src/main/java/br/com/andrebg/apendendo/notation/ClienteReflection.java
@@ -1,9 +1,37 @@
 package br.com.andrebg.apendendo.notation;
 
-public class ClienteReflection {
+import org.apache.commons.lang3.ArrayUtils;
 
-	public static void listagem2() {
+public class ClienteReflection {
+	
+	public static void listagem_2() {
 		Cliente cliente = new Cliente();
-        System.out.println(cliente.getClass().getName());
+		
+		System.out.println("Imprimindo resultado da Listagem 2:\n");
+		
+        System.out.println(cliente.getClass().getName());//Código original do artigo 
+	}
+
+	public static void listagem_2a() {
+		Cliente cliente = new Cliente();
+		
+		System.out.println("Imprimindo resultado da Listagem 2a:\n");
+		
+        System.out.println(cliente.getClass().getName());//Código original do artigo 
+        
+        
+        //Código adicional de manipulação de String, não essencial para compeenção do Artigo
+        System.out.println(cliente.getClass());
+        System.out.println(cliente);
+        
+        String partes[] = cliente.getClass().getName().split("\\.");
+        String Class = partes[partes.length-1];
+		System.out.println("Class:  \t" + Class);
+		
+		
+		partes = ArrayUtils.removeElement(partes, Class);	
+		System.out.println("package:\t" + String.join(".", partes));
+        
+        System.out.println("\n\n");
 	}
 }
